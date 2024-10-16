f6d86a7484024dc1041489aa5555a9db0fecf127
eduardob1503 <eticleo@yahoo.com.br>
2024-10-16 14:37:14 -0300

    termino dos menus

--- lib/pages/materias.dart
+++ lib/pages/materias.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:ws_project/pages/ranking.dart';
+import 'perfil.dart';
 
 class Materias extends StatelessWidget{
   const Materias ({super.key});
@@ -24,12 +26,113 @@ class TelaMaterias extends StatelessWidget{
             return IconButton(
               icon: const Icon(Icons.menu),
               onPressed: (){
-
+            Scaffold.of(context).openDrawer();
             }, 
             );
           }
         ),
       ),
-    );
+        drawer: Drawer(
+        backgroundColor: Colors.white,
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.white,
+              ),
+              child: Image.network(
+              'https://via.placeholder.com/150', // URL do seu logo ou use Image.asset se estiver na pasta do projeto
+              height: 100,
+            ),
+        ),
+        Container(
+              decoration: BoxDecoration(
+                border: Border.all(
+                  color: Colors.black,
+                  width: 1
+                )
+              ),
+
+            child: ListTile(
+              tileColor: const Color.fromARGB(255, 255, 255, 255),
+              title: const Center(
+                child: Text("Meu Perfil",
+                style: TextStyle(color: Colors.black),
+                ),
+              ),
+              onTap: (){
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const TelaPerfil(),
+                    )
+                  );
+              },
+            ),
+            ),
+
+            Container(
+              decoration: BoxDecoration(
+                border: Border.all(
+                  color: Colors.black,
+                  width: 1
+                )
+              ),
+
+            child: ListTile(
+              tileColor: const Color.fromARGB(255, 255, 255, 255),
+              title: const Center(
+                child: Text(
+                  "Ranking",
+                style: TextStyle(color: Color.fromARGB(255, 0, 0, 0)),
+                ),
+              ),
+              onTap: (){
+Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const TelaRanking(),
+                    )
+                  );
+              },
+            ),
+            ),
+            Container(
+              decoration: BoxDecoration(
+                border: Border.all(
+                  color: Colors.black,
+                  width: 1
+                )
+              ),
+
+            child: ListTile(
+              tileColor: const Color.fromARGB(255, 0, 0, 0),
+              title: const Center(
+                child: Text(
+                  "Matéria",
+                style: TextStyle(color: Color.fromARGB(255, 255, 255, 255)),
+                ),
+              ),
+              onTap: (){
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const TelaMaterias(),
+                    )
+                  );
+              },
+            ),
+            ),
+      ],
+      )
+    ),
+
+
+
+
+
+
+  );   
   }
 }
--- lib/pages/ranking.dart
+++ lib/pages/ranking.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:ws_project/pages/materias.dart';
+import 'package:ws_project/pages/perfil.dart';
 
 class Ranking extends StatelessWidget{
   const Ranking ({super.key});
@@ -18,13 +20,113 @@ class TelaRanking extends StatelessWidget{
   Widget build(BuildContext context){
     return Scaffold(
       appBar: AppBar(
-        leading: IconButton(
+        leading: Builder(
+          builder:(context) {
+        return IconButton(
           onPressed: (){
 
+            Scaffold.of(context).openDrawer();
           },
-         icon: const Icon(Icons.menu)
-         ),
+           icon: const Icon(Icons.menu)
+         );
+      
+        },
+        ),
       ),
+      drawer: Drawer(
+        backgroundColor: Colors.white,
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            DrawerHeader(
+              decoration: const BoxDecoration(
+                color: Colors.white,
+              ),
+              child: Image.network("https://via.placeholder.com/150",
+              height:100,
+              ),
+            ),
+
+            Container(
+              decoration: BoxDecoration(
+                border: Border.all(
+                  color: Colors.black,
+                  width: 1
+                )
+              ),
+
+            child: ListTile(
+              tileColor: const Color.fromARGB(255, 255, 255, 255),
+              title: const Center(
+                child: Text("Meu Perfil",
+                style: TextStyle(color: Colors.black),
+                ),
+              ),
+              onTap: (){
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const TelaPerfil(),
+                    )
+                  );
+              },
+            ),
+            ),
+
+            Container(
+              decoration: BoxDecoration(
+                border: Border.all(
+                  color: Colors.black,
+                  width: 1
+                )
+              ),
+
+            child: ListTile(
+              tileColor: const Color.fromARGB(255, 0, 0, 0),
+              title: const Center(
+                child: Text(
+                  "Ranking",
+                style: TextStyle(color: Color.fromARGB(255, 255, 255, 255)),
+                ),
+              ),
+              onTap: (){
+
+              },
+            ),
+            ),
+            Container(
+              decoration: BoxDecoration(
+                border: Border.all(
+                  color: Colors.black,
+                  width: 1
+                )
+              ),
+
+            child: ListTile(
+              tileColor: const Color.fromARGB(255, 252, 252, 252),
+              title: const Center(
+                child: Text(
+                  "Matéria",
+                style: TextStyle(color: Colors.black),
+                ),
+              ),
+              onTap: (){
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const TelaMaterias(),
+                    )
+                  );
+              },
+            ),
+            ),
+
+
+
+          ],
+        ),
+      ),
+
       body: const SingleChildScrollView(
         padding: EdgeInsets.all(16.0),
         child: Column(
