5da60446175333b75d1069bfc2a9d30235975fe4
gabrielmorgado0 <gabrielmorgado02@yahoo.com>
2024-10-16 14:43:37 -0300

    tela materias

--- lib/pages/materias.dart
+++ lib/pages/materias.dart
@@ -1,34 +1,61 @@
 import 'package:flutter/material.dart';
 
-class Materias extends StatelessWidget{
-  const Materias ({super.key});
+class TelaMaterias extends StatelessWidget {
+  const TelaMaterias({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context){
-    return MaterialApp(
-      home: TelaMaterias(),
-    );
-  }
-}
-
-class TelaMaterias extends StatelessWidget{
-  const TelaMaterias ({super.key});
-
-  @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Scaffold(
+      // AppBar com menu e pesquisa
       appBar: AppBar(
+        leading: IconButton(icon: Icon(Icons.menu), onPressed: () {}),
+        actions: [IconButton(icon: Icon(Icons.search), onPressed: () {})],
         backgroundColor: Colors.white,
-        leading: Builder(
-          builder: (context){
-            return IconButton(
-              icon: const icon(Icons.menu),
-              onPressed: (){
-
-            }, )
-          }
+        iconTheme: IconThemeData(color: Colors.black),
+      ),
+      // Lista de matérias
+      body: ListView.builder(
+        itemCount: 10,
+        itemBuilder: (context, index) => Padding(
+          padding: EdgeInsets.all(16),
+          child: _buildMateriaCard(index),
         ),
       ),
-    )
+    );
+  }
+
+  // Método para construir o card de cada matéria
+  Widget _buildMateriaCard(int index) {
+    return Card(
+      child: SizedBox(
+        height: 140, // Altura fixa de 140px
+        child: Stack(
+          children: [
+            // Nome da matéria (canto superior esquerdo)
+            Positioned(
+              top: 16,
+              left: 16,
+              child: Text(
+                'Nome da Matéria ${index + 1}',
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+            ),
+            // Botão de acesso (canto inferior direito)
+            Positioned(
+              bottom: 16,
+              right: 16,
+              child: ElevatedButton(
+                child: Text('Acessar'),
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.black,
+                  foregroundColor: Colors.white,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
