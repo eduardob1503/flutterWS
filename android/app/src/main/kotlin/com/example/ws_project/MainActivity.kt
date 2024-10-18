f5bcdfef24537cfb679bd006fa17e6110252cfd0
unknown <eticleo@yahoo.com.br>
2024-10-18 15:57:06 -0300

    Mudanças

--- android/app/src/main/kotlin/com/example/ws_project/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/ws_project/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.example.biometria
 
 import io.flutter.embedding.android.FlutterFragmentActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
 
-class MainActivity : FlutterFragmentActivity() {
-    // Você pode adicionar código adicional aqui, se necessário
+class MainActivity: FlutterFragmentActivity() {
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine)
+    }
 }
